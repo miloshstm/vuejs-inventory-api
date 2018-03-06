1bd5ea605d6c7249ba24535471b2edbfa30e1200
George <dexor2@yahoo.com>
2018-03-06 22:02:26 +0100

    Updates to MainJS for the local JSON file

--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
-// VueJs inventory using a JSON API
-const app = new Vue({
-    el: '#app',
-    data: {
-        products: []
-    },
-    computed: {
-        totalProducts() {
-            return this.products.reduce((sum, product) => { 
-                return sum + product.quantity;
-             }, 0)
-        }
-    },
-    created() {
-        fetch('https://api.myjson.com/bins/nf3r3')
-        .then(res => res.json())
-        .then(json => this.products = json.products)
-    }
-})
+(function() {
+	// VueJs inventory using a JSON API
+	const app = new Vue({
+	    el: '#app',
+	    data: {
+	        products: []
+	    },
+	    computed: {
+	        totalProducts() {
+	            return this.products.reduce((sum, product) => { 
+	                return sum + product.quantity;
+	             }, 0)
+	        }
+	    },
+	    created() {
+	        fetch('https://api.myjson.com/bins/nf3r3')
+		    /* Uncomment the line bellow to enable 
+		    the local JSON file(if CDN isn't working) */
+		    // fetch('books.json')   
+	        .then(res => res.json())
+	        .then(json => this.products = json.products)
+	    }
+	})
 
-// Displays the JSON API of the above Vue implementation
-function printBooks() {
-    fetch('https://api.myjson.com/bins/nf3r3')
-    .then(res => res.json())
-    .then(data => {
-        let display = '';
-        data.products.forEach(book => {
-            display += `
-                <ul class="list-group mb-3">
-                    <li class="list-group-item list-group-item-action list-group-item-light">
-                        <strong>ID:</strong><code>&nbsp;${book.id}</code>
-                    </li>
-                    <li class="list-group-item list-group-item-action list-group-item-light">
-                        <strong>Quantity:</strong><code>&nbsp;${book.quantity}</code>
-                    </li>
-                    <li class="list-group-item list-group-item-action list-group-item-light">
-                        <strong>Name:</strong><code>&nbsp;${book.name}</code>
-                    </li>
-                </ul>
-            `;
-        });
-        document.getElementById('display').innerHTML = display;
-    })
-}
-document.getElementById('printBooks').addEventListener('click', printBooks);
+	// Displays the JSON API of the above Vue implementation
+	function printBooks() {
+	    fetch('https://api.myjson.com/bins/nf3r3')
+	    // fetch('books.json') 
+	    .then(res => res.json())
+	    .then(data => {
+	        let display = '';
+	        data.products.forEach(book => {
+	            display += `
+	                <ul class="list-group mb-3">
+	                    <li class="list-group-item list-group-item-action list-group-item-light">
+	                        <strong>ID:</strong><code>&nbsp;${book.id}</code>
+	                    </li>
+	                    <li class="list-group-item list-group-item-action list-group-item-light">
+	                        <strong>Quantity:</strong><code>&nbsp;${book.quantity}</code>
+	                    </li>
+	                    <li class="list-group-item list-group-item-action list-group-item-light">
+	                        <strong>Name:</strong><code>&nbsp;${book.name}</code>
+	                    </li>
+	                </ul>
+	            `;
+	        });
+	        document.getElementById('display').innerHTML = display;
+	    })
+	}
+	document.getElementById('printBooks').addEventListener('click', printBooks);
+})();
